ef8b177a42f08d66543b347c85d386a81233ba94
Nathan Henrie <nate@n8henrie.com>
2015-10-17 08:34:09 -0600

    Add public HTML file url to top of Markdown file.
    The code will now derive the public link to the HTML file and put it
    as an HTML comment at the top of the Markdown file. This way you can
    access the url directly from editing the markdown, but it won't show up
    in the final HTML.

--- app.py
+++ app.py
@@ -6,22 +6,24 @@ import threading
 import urlparse
 
 from dropbox.client import DropboxClient, DropboxOAuth2Flow
-from flask import abort, Flask, redirect, render_template, request, session, url_for
+from flask import abort, Flask, redirect, render_template, request, session,\
+                  url_for
 from markdown import markdown
 import redis
- 
+
 redis_url = os.environ['REDISTOGO_URL']
 redis_client = redis.from_url(redis_url)
- 
+
 # App key and secret from the App console (dropbox.com/developers/apps)
 APP_KEY = os.environ['APP_KEY']
 APP_SECRET = os.environ['APP_SECRET']
- 
+
 app = Flask(__name__)
 app.debug = True
- 
+
 # A random secret used by Flask to encrypt session data cookies
 app.secret_key = os.environ['FLASK_SECRET_KEY']
+
 
 def get_url(route):
     '''Generate a proper URL, forcing HTTPS if not running locally'''
@@ -34,6 +36,7 @@ def get_url(route):
 
     return url
 
+
 def get_flow():
     return DropboxOAuth2Flow(
         APP_KEY,
@@ -42,23 +45,28 @@ def get_flow():
         session,
         'dropbox-csrf-token')
 
+
 @app.route('/welcome')
 def welcome():
-    return render_template('welcome.html', redirect_url=get_url('oauth_callback'),
-        webhook_url=get_url('webhook'), home_url=get_url('index'), app_key=APP_KEY)
+    return render_template('welcome.html',
+                           redirect_url=get_url('oauth_callback'),
+                           webhook_url=get_url('webhook'),
+                           home_url=get_url('index'), app_key=APP_KEY)
+
 
 @app.route('/oauth_callback')
 def oauth_callback():
     '''Callback function for when the user returns from OAuth.'''
 
     access_token, uid, extras = get_flow().finish(request.args)
- 
+
     # Extract and store the access token for this user
     redis_client.hset('tokens', uid, access_token)
 
     process_user(uid)
 
     return redirect(url_for('done'))
+
 
 def process_user(uid):
     '''Call /delta for the given user ID and process any changes.'''
@@ -84,8 +92,23 @@ def process_user(uid):
                 continue
 
             # Convert to Markdown and store as <basename>.html
-            html = markdown(client.get_file(path).read())
-            client.put_file(path[:-3] + '.html', html, overwrite=True)
+            response, metadata = client.get_file_and_metadata(path)
+            md = response.read()
+            html = markdown(md)
+            html_name = path[:-3] + '.html'
+            client.put_file(html_name, html, overwrite=True)
+
+            # Include URL to published file in HTML comment at top of Markdown
+            # file
+            if '<!-- Published file url:' != md.split('\n')[0]:
+                share_url = client.share(html_name, short_url=False).get('url')
+                file_key = share_url.split('/')[4]
+
+                url_comment = ('<!-- Published file url:\n'
+                               'https://dl.dropboxusercontent.com/s/'
+                               '{}{}\n-->\n'.format(file_key, html_name))
+                md = url_comment + md
+                client.put_file(path, md, overwrite=True)
 
         # Update cursor
         cursor = result['cursor']
@@ -94,17 +117,21 @@ def process_user(uid):
         # Repeat only if there's more to do
         has_more = result['has_more']
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/login')
 def login():
     return redirect(get_flow().start())
 
+
 @app.route('/done')
-def done(): 
+def done():
     return render_template('done.html')
+
 
 def validate_request():
     '''Validate that the request is properly signed by Dropbox.
@@ -113,18 +140,22 @@ def validate_request():
     signature = request.headers.get('X-Dropbox-Signature')
     return signature == hmac.new(APP_SECRET, request.data, sha256).hexdigest()
 
+
 @app.route('/webhook', methods=['GET'])
 def challenge():
-    '''Respond to the webhook challenge (GET request) by echoing back the challenge parameter.'''
+    '''Respond to the webhook challenge (GET request) by echoing back the
+    challenge parameter.'''
 
     return request.args.get('challenge')
+
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     '''Receive a list of changed user IDs from Dropbox and process each.'''
 
     # Make sure this is a valid request from Dropbox
-    if not validate_request(): abort(403)
+    if not validate_request():
+        abort(403)
 
     for uid in json.loads(request.data)['delta']['users']:
         # We need to respond quickly to the webhook request, so we do the
@@ -134,5 +165,5 @@ def webhook():
         threading.Thread(target=process_user, args=(uid,)).start()
     return ''
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
